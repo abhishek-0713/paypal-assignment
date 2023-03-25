aac8321624d6c3246c6071eaee8809b3cb5be3f7
Coder Abhi <coder.abhi0713@gmail.com>
2023-03-25 11:09:28 +0530

    Sprint controller added

--- Paypal-assignment/src/main/java/com/paypal/controller/SprintController.java
+++ Paypal-assignment/src/main/java/com/paypal/controller/SprintController.java
@@ -17,5 +17,55 @@ import java.util.List;
 @RequestMapping("/paypal")
 public class SprintController {
 
+    @Autowired
+    private SprintService sprintService;
+
+    @PostMapping("/sprint")
+    public ResponseEntity<Sprint> createSprint(@RequestBody Sprint sprint) {
+
+        Sprint newSprint = sprintService.createSprint(sprint);
+        return new ResponseEntity<>(newSprint, HttpStatus.CREATED);
+
+    }
+
+    @PutMapping("/task")
+    public ResponseEntity<?> addTaskToSprint(@RequestParam Integer sprintId, @RequestBody Task task) throws ResourceNotFoundException {
+
+        Sprint sprint = sprintService.addTaskToSprint(sprintId, task);
+        return new ResponseEntity<>(sprint, HttpStatus.ACCEPTED);
+    }
+
+    @PutMapping("/change/assignee")
+    public ResponseEntity<?> changeAssignee(@RequestParam Integer taskId, @RequestBody User user) throws ResourceNotFoundException {
+
+        Sprint sprint = sprintService.changeAssignee(taskId, user);
+        return new ResponseEntity<>(sprint, HttpStatus.ACCEPTED);
+
+    }
+
+    @PutMapping("/task/changeStatus")
+    public ResponseEntity<?> changeStatus(@RequestParam Integer taskId, @RequestParam TaskStatus status) throws ResourceNotFoundException {
+
+        Sprint sprint = sprintService.changeStatus(taskId, status);
+        return new ResponseEntity<>(sprint, HttpStatus.ACCEPTED);
+
+    }
+
+    @GetMapping("/tasks/sprintId")
+    public  ResponseEntity<List<Task>> getAllTasksBySprintId(@RequestParam Integer sprintId) throws ResourceNotFoundException{
+
+        List<Task> tasks = sprintService.getAllTasksBySprintId(sprintId);
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/tasks/assigneeId")
+    public  ResponseEntity<List<Task>> getAllTasksByAssigneeId(@RequestParam Integer assigneeId) throws ResourceNotFoundException{
+
+        List<Task> tasks = sprintService.getAllTasksByAssigneeId(assigneeId);
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+
+    }
+
 
 }
