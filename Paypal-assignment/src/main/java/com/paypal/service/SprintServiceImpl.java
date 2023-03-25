58b7bfdd5a56fb8404b429692df4aab59fbf8e98
Coder Abhi <coder.abhi0713@gmail.com>
2023-03-25 11:00:13 +0530

    entity and service implementation added

--- Paypal-assignment/src/main/java/com/paypal/service/SprintServiceImpl.java
+++ Paypal-assignment/src/main/java/com/paypal/service/SprintServiceImpl.java
@@ -14,8 +14,62 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class SprintServiceImpl {
+public class SprintServiceImpl implements SprintService{
 
 
+        @Autowired
+        private UserRepository userRepository;
+
+        @Autowired
+        private SprintRepository sprintRepository;
+
+        @Autowired
+        private TaskRepository taskRepository;
+
+        @Override
+        public Sprint createSprint(Sprint sprint) {
+            return sprintRepository.save(sprint);
+        }
+
+        @Override
+        public Sprint addTaskToSprint(Integer sprintId, Task task) throws ResourceNotFoundException {
+            Sprint sprint = sprintRepository.findById(sprintId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Sprint not found with id " + sprintId));
+            task.setSprint(sprint);
+            taskRepository.save(task);
+            return sprint;
+        }
+
+        @Override
+        public Sprint changeAssignee(Integer taskId, User user) throws ResourceNotFoundException {
+            Task task = taskRepository.findById(taskId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Task not found with id " + taskId));
+            task.setAssignee(user);
+            taskRepository.save(task);
+            return null;
+        }
+
+        @Override
+        public Sprint changeStatus(Integer taskId, TaskStatus status) throws ResourceNotFoundException {
+            Task task = taskRepository.findById(taskId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Task not found with id " + taskId));
+            task.setStatus(status);
+            taskRepository.save(task);
+            return null;
+        }
+
+        @Override
+        public List<Task> getAllTasksBySprintId(Integer sprintId) throws ResourceNotFoundException {
+            Sprint sprint = sprintRepository.findById(sprintId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Sprint not found with id " + sprintId));
+            return sprint.getTasks();
+        }
+
+        @Override
+        public List<Task> getAllTasksByAssigneeId(Integer assigneeId) throws ResourceNotFoundException {
+            User user = userRepository.findById(assigneeId)
+                    .orElseThrow(() -> new ResourceNotFoundException("User not found with id " + assigneeId));
+            return user.getTasks();
+        }
 
 }
